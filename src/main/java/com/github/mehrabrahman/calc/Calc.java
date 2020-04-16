1a8c75d5814c05ba3014f3cab8d78debaffdca83
Mehrab <Mehrab.Rahman@Revature.com>
2020-04-16 11:41:40 -0500

    Refactoring to OOP

--- src/main/java/com/github/mehrabrahman/calc/Calc.java
+++ src/main/java/com/github/mehrabrahman/calc/Calc.java
@@ -3,46 +3,10 @@ package com.github.mehrabrahman.calc;
 import com.github.mehrabrahman.calc.math.Operation;
 
 class Calc {
-    public Operation parse(String[] args) {
-        Operation operation = new Operation();
-        String subcommand = "";
-        double a = 0.0;
-        double b = 0.0;
-        if (args.length > 0) {
-            subcommand = args[0];
-            try {
-                a = Double.parseDouble(args[1]);
-                b = Double.parseDouble(args[2]);
-                operation = new Operation(subcommand, a, b);
-            } catch (NumberFormatException e) {
-                System.out.println("Numbers only!");
-                System.exit(1);
-            }
-        }
-        return operation;
-    }
-
     public static void main(String[] args) {
-        // Parse input: 1) parse subcommand, 2) parse operands        
-        Calc calc = new Calc();
-        Operation operation = calc.parse(args);
-
-        // Select operation
-        double output = 0.0;
-        if (operation.subcommand.equals("add")) {
-            // Perform operation
-            output = operation.a + operation.b;
-        } else if (operation.subcommand.equals("sub")) {
-            output = operation.a - operation.b;
-        } else if (operation.subcommand.equals("mul")) {
-            output = operation.a * operation.b;
-        } else if (operation.subcommand.equals("div")) {
-            output = operation.a / operation.b;
-        } else {
-            System.out.println("Unrecognized");
-        }
-
+        // Parse input: 1) parse subcommand, 2) parse operands  
+        Operation operation = new Operation(args);
         // Output
-        System.out.println(output);
+        System.out.println(operation);
     }
 }
--- src/main/java/com/github/mehrabrahman/calc/math/Operation.java
+++ src/main/java/com/github/mehrabrahman/calc/math/Operation.java
@@ -1,15 +1,47 @@
 package com.github.mehrabrahman.calc.math;
 
+/**
+This class holds data for an operation.
+@author Mehrab
+@version 0.1.0
+*/
 public class Operation {
-    public String subcommand;
-    public double a;
-    public double b;
+    private String subcommand;
+    private double a;
+    private double b;
+    private double output;
 
-    public Operation(String subcommand, double a, double b) {
-        this.subcommand = subcommand;
-        this.a = a;
-        this.b = b;
+    public Operation(String[] args) {
+        if (args.length > 0) {
+            this.subcommand = args[0];
+            try {
+                this.a = Double.parseDouble(args[1]);
+                this.b = Double.parseDouble(args[2]);
+                this.output = this.output();
+            } catch (NumberFormatException e) {
+                System.out.println("Numbers only!");
+                System.exit(1);
+            }
+        }
     }
 
-    public Operation() {}
+    private double output() {
+        switch (subcommand) {
+            case "add":
+                return this.a + this.b;
+            case "sub":
+                return this.a - this.b;
+            case "mul":
+                return this.a * this.b;
+            case "div":
+                return this.a / this.b;
+            default:
+                return 0.0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.a + " + " + this.b + " = " + this.output;
+    }
 }
